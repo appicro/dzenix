73a6ffda9152d906861e757ebb2b6aacc876d84e
appicro <75711026+appicro@users.noreply.github.com>
2021-01-07 09:51:52 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -494,7 +494,7 @@ var pulldown = $(".page-area-linkbox").find(".page-area-linkbox__list:first");
   infinite: true,
   speed: 300,
   slidesToShow: 1,
-        autoplay: ture,
+        autoplay: true,
         autoplaySpeed: 2000,	    
   nextArrow: '.slider-next',
   prevArrow: '.slider-prev',
